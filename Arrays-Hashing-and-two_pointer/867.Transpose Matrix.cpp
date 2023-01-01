ce8041bbca13ecf9c40d1ebae46a869676cf9d62
reza <rahmanreza646@gmail.com>
2023-01-02 01:36:36 +0600

    first commit

---
+++ Arrays-Hashing-and-two_pointer/867.Transpose Matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        vector<vector<int>> ans;
+
+        for(int i=0;i<matrix[0].size();i++)
+        {
+            vector<int> val;
+            for(int j=0;j<matrix.size();j++)
+            {
+                val.push_back(matrix[j][i]);
+
+            }
+            ans.push_back(val);
+        }
+
+        return ans;
+    }
+};
